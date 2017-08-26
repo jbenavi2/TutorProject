43b77e2a3667c88c34839659edaece65e882afbb
Javier <jbenavi2@asu.edu>
2017-08-26 13:57:47 -0700

    Initial checkin

---
+++ TutorProject/src/Universe.java
@@ -0,0 +1,11 @@
+
+public class Universe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("Hi again");
+
+	}
+
+}
