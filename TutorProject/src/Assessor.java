706d02d293f962573861dc1e0048fc2e78fc9385
Javier <Javier@Echoblast>
2017-09-01 01:15:03 -0700

    change size and alignment of buttons

--- TutorProject/src/Assessor.java
+++ TutorProject/src/Assessor.java
@@ -10,7 +10,10 @@ public class Assessor extends JPanel{
 	int state = 0;
 	
 	// The panels for questions
-	private JPanel question3;
+	private JPanel panelQuestion3;
+	
+	// The title labels for each panel
+	private JLabel labelQuestion3;
 	
 	// buttons for Question 3
 	private JButton option1, option2, option3;
@@ -24,18 +27,27 @@ public class Assessor extends JPanel{
 		// if state = 0 <Javier>
 		
 		if(state == 3) {
-			option1 = new JButton("option 1");
-			option2 = new JButton("option 2");
-			option3 = new JButton("option 3");
+			//create my label
+			labelQuestion3 = new JLabel("Question 3");
 			
-			question3 = new JPanel();
-			question3.setLayout(new GridLayout (3,1));
-			question3.add(option1);
-			question3.add(option2);
-			question3.add(option3);
+			//create my 3 option buttons
+			option1 = new JButton("option 1");
+			option1.setAlignmentX(Component.CENTER_ALIGNMENT);
+			option2 = new JButton("option 2");
+			option2.setAlignmentX(Component.CENTER_ALIGNMENT);
+			option3 = new JButton("option 3");
+			option3.setAlignmentX(Component.CENTER_ALIGNMENT);
+			
+			panelQuestion3 = new JPanel();
+			panelQuestion3.setLayout(new BoxLayout(panelQuestion3, BoxLayout.Y_AXIS));
+			panelQuestion3.add(labelQuestion3);
+			//panelQuestion3.add(Box.createRigidArea(new Dimension(0,5)));
+			panelQuestion3.add(option1);
+			panelQuestion3.add(option2);
+			panelQuestion3.add(option3);
 			
 			setLayout(new BorderLayout());
-			add(question3);
+			add(panelQuestion3);
 		}		
 		
 	}	
