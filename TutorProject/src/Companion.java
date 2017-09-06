e936aaacf89496fa7af68e41a6b31c2cf35a65b7
ndepaula <31515801+ndepaula@users.noreply.github.com>
2017-09-05 23:06:31 -0700

    Buttons
    Added a few radio buttons just for set-up

--- TutorProject/src/Companion.java
+++ TutorProject/src/Companion.java
@@ -1,7 +1,20 @@
 //Natalie DePaula
-public class Companion {
-
-	//this is a change...
+public class Companion extends JPanel {
 	
+	private JRadioButton A, B, C, D;
+	
+	public Companion() {
+	
+		group = new ButtonGroup();
+		group.add(A);
+		group.add(B);
+		group.add(C);
+		group.add(D);
+		A = new JRadioButton ("A");
+		B = new JRadioButton ("B");
+		C = new JRadioButton ("C");
+		D = new JRadioButton ("D");
+		
+	}
 	
 }
