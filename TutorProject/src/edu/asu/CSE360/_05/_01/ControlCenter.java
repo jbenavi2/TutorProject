2ce74532b904882f58a38b2ff6646dd086f9eb07
Javier <Javier@Echoblast>
2017-11-05 18:39:59 -0700

    made ControlCenter into Singleton type

--- TutorProject/src/edu/asu/CSE360/_05/_01/ControlCenter.java
+++ TutorProject/src/edu/asu/CSE360/_05/_01/ControlCenter.java
@@ -1,5 +1,23 @@
 package edu.asu.CSE360._05._01;
 
 public class ControlCenter {
+	
+	private double[] array1;
+	private double[] array2;
+	private double[] array3;
+	private static ControlCenter _instance;
+	
+	private ControlCenter() {
+		
+	}
+	
+	public static ControlCenter getInstance() {
+		if(_instance == null) {
+			_instance = new ControlCenter();
+		}
+		return _instance;
+	}
+	
+	
 
 }
