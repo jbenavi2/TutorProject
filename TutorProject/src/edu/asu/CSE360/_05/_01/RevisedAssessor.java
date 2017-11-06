bb213d37c3b3c0becaac79a4e555f7b6fd9f1d97
Javier <jbenavi2@asu.edu>
2017-11-06 14:49:56 -0700

    Started RevisedAssessor

---
+++ TutorProject/src/edu/asu/CSE360/_05/_01/RevisedAssessor.java
@@ -0,0 +1,34 @@
+package edu.asu.CSE360._05._01;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.TimeUnit;
+import javax.swing.*;
+
+public class RevisedAssessor extends JPanel{
+	int score = 0;
+	
+	CompanionPanel companion = new CompanionPanel();
+	
+	private JPanel defaultPanel;
+	private JPanel panelQuestion;
+	
+	private JLabel labelQuestion;
+	
+	private JRadioButton RBoption1, RBoption2, RBoption3;
+	
+	//default constructor
+	public RevisedAssessor() {
+		
+		defaultPanel = new JPanel();
+		defaultPanel.setLayout(new GridLayout(1,2));
+		
+		//ask user if they are ready
+		labelQuestion = new JLabel("ARE YOU READY?");
+		labelQuestion.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		
+		
+	}
+
+}
